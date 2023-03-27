75751866488fcfea56f62a9cbf31cdda8226206f
Isaac Machakata <108731754+im-machakata@users.noreply.github.com>
2023-03-27 15:46:46 +0200

    refactor: components

---
+++ js/Header.vue
@@ -0,0 +1,3 @@
+<template>
+  <div class="menu">Awesome Menu Here</div>
+</template>
---
+++ js/Menu.vue
@@ -0,0 +1,10 @@
+<template></template>
+
+<script>
+export default {
+  name: 'MenuComponent',
+  // Your component options go here
+}
+// Export the component globally
+window.MenuCoMenuComponent =MenuComponent
+</script>

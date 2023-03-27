165c1dd6140d16be7e9f297b2d2e22be1c1ce9ad
Isaac Machakata <108731754+im-machakata@users.noreply.github.com>
2023-03-27 16:20:02 +0200

    refactor: footer

---
+++ js/Footer.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="menu">Awesome Menu Here</div>
+</template>
+
+<script>
+export default {
+  name: 'FooterComponent',
+  // Your component options go here
+}
+// Export the component globally
+window.FooterCoMenuComponent =FooterComponent
+</script>

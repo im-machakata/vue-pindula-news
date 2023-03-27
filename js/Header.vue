017be9c167be82ecca7a0c1c61c51d2a197c12c6
Isaac Machakata <108731754+im-machakata@users.noreply.github.com>
2023-03-27 16:18:28 +0200

    refactor: header component

--- js/Header.vue
+++ js/Header.vue
@@ -1,3 +1,12 @@
 <template>
   <div class="menu">Awesome Menu Here</div>
 </template>
+
+<script>
+export default {
+  name: 'HeaderComponent',
+  // Your component options go here
+}
+// Export the component globally
+window.HeaderCoMenuComponent =HeaderComponent
+</script>

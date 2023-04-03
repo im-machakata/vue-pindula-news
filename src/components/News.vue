15dee1f13fa2ad7eba606a5967cc50a7271bf9d9
Isaac Machakata <108731754+im-machakata@users.noreply.github.com>
2023-04-03 06:32:00 +0200

    style: loader refactor: fetchNews

--- src/components/News.vue
+++ src/components/News.vue
@@ -1,12 +1,21 @@
 <template>
   <p v-show="news.length < 1 && !connection_error" class="text-white mt-5 px-3 pt-3 lg:text-center">   
-    Loading ...
+    <div aria-label="Loading..." role="status">
+      <svg class="h-6 w-6 animate-spin" viewBox="3 3 18 18">
+        <path
+          class="fill-indigo-200"
+          d="M12 5C8.13401 5 5 8.13401 5 12C5 15.866 8.13401 19 12 19C15.866 19 19 15.866 19 12C19 8.13401 15.866 5 12 5ZM3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12Z"></path>
+        <path
+          class="fill-indigo-800"
+          d="M16.9497 7.05015C14.2161 4.31648 9.78392 4.31648 7.05025 7.05015C6.65973 7.44067 6.02656 7.44067 5.63604 7.05015C5.24551 6.65962 5.24551 6.02646 5.63604 5.63593C9.15076 2.12121 14.8492 2.12121 18.364 5.63593C18.7545 6.02646 18.7545 6.65962 18.364 7.05015C17.9734 7.44067 17.3403 7.44067 16.9497 7.05015Z"></path>
+      </svg>
+    </div>
   </p>
   <p v-show="connection_error && news.length < 1" class="text-white mt-5 px-3 pt-3 lg:text-center">
     An error occured connecting to the server. <a @click="fetchNews()">Retry</a>
   </p>
   <section class="news-list">
-    <div v-for="item in news" :class="(item.is_open ? '' : 'closed' ) + 'news-item'" :key="item.id">
+    <div v-for="item in news" :class="(item.is_open ? '' : 'closed' ) + 'news-item'" :key="item.id" @click="item.is_open = !item.is_open">
       <img class="news-image" v-bind:src="item.image_src">
       <div>
         <a class="news-title" v-bind:title="item.title" target="_blank" v-bind:href="'//zero.pindula.co.zw/'+item.slug">{{ item.title }}</a>
@@ -24,7 +33,7 @@ export default {
       fetch(atob(this.news_url))
         .then(response => response.json())
         .then(response => {
-          this.news = response.results.array.forEach(element => {
+          this.news = response.results.array.map(element => {
             element.is_open = false;
           });
           this.connection_error = false;

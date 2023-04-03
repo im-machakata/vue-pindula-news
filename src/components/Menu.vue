e1561a5d826299a807d6e1a5191903684dfff1e7
Isaac Machakata <isaac@caasi.co.zw>
2023-04-03 19:25:40 +0200

    refactor: menu

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,17 +1,18 @@
 <template>
-    <nav class="mx-auto shadow-md border-y-2 border-blue-700 bg-blue-800 w-full py-4">
-        <ul class="flex flex-row justify-around items-center lg:mr-10 font-semibold w-full font-sans cursor-pointer">
-            <li v-for="item in items" class="px-2 text-white"><a v-bind:href="item.href" target="_b" @click="menuClicked($event,item.text)">{{item.text}}</a></li>
-        </ul>
-    </nav>
+  <nav class="mx-auto shadow-md border-y-2 border-blue-700 bg-blue-800 w-full py-4">
+    <ul class="flex flex-row justify-around items-center lg:mr-10 font-semibold w-full font-sans cursor-pointer">
+      <li v-for="item in items" class="px-2 text-white"><a v-bind:href="item.href" target="_b"
+          @click="menuClicked($event, item.text)">{{ item.text }}</a></li>
+    </ul>
+  </nav>
 </template>
 <script>
 export default {
   name: 'Menu',
   methods: {
     menuClicked: (e, text) => {
-      if(text == "Home"){
-      e.preventDefault();
+      if (text == "Home") {
+        e.preventDefault();
       }
     },
   },
@@ -19,16 +20,16 @@ export default {
     return {
       items: [
         {
-            text: 'Home',
-            href: '/',
+          text: 'News',
+          href: '/',
         },
         {
-            text: 'Jobs',
-            href: '//zero.pindula.co.zw/jobs/',
+          text: 'Jobs',
+          href: '//zero.pindula.co.zw/jobs/',
         },
         {
-            text: 'Market',
-            href: '//zero.pindula.co.zw/market/',
+          text: 'Market',
+          href: '//zero.pindula.co.zw/market/',
         },
       ],
       msg: null

b8fe3c85f3bbd780efff15473c9f8d5441f45615
Isaac Machakata <isaac@caasi.co.zw>
2023-04-04 05:19:39 +0200

    style: delay items

--- src/components/Loader.vue
+++ src/components/Loader.vue
@@ -1,8 +1,8 @@
 <template>
 	<section class="flex items-center justify-center space-x-2 my-10">
 		<div class="w-4 h-4 rounded-full animate-pulse bg-white dark:bg-blue-200"></div>
-		<div class="w-4 h-4 rounded-full animate-pulse bg-white dark:bg-blue-200"></div>
-		<div class="w-4 h-4 rounded-full animate-pulse bg-white dark:bg-blue-200"></div>
+		<div class="w-4 h-4 rounded-full animate-pulse delay-75 bg-white dark:bg-blue-200"></div>
+		<div class="w-4 h-4 rounded-full animate-pulse delay-150 bg-white dark:bg-blue-200"></div>
 	</section>
 </template>
 <script>

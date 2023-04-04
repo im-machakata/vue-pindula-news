d7756ee3e97b26f3468cf8ffcb2f2dbbb7534b70
Isaac Machakata <isaac@caasi.co.zw>
2023-04-04 04:47:54 +0200

    refactor: loader

--- src/components/Loader.vue
+++ src/components/Loader.vue
@@ -1,5 +1,5 @@
 <template>
-	<section class="flex items-center justify-center space-x-2 mt-10">
+	<section class="flex items-center justify-center space-x-2 my-10">
 		<div class="w-4 h-4 rounded-full animate-pulse bg-white dark:bg-blue-200"></div>
 		<div class="w-4 h-4 rounded-full animate-pulse bg-white dark:bg-blue-200"></div>
 		<div class="w-4 h-4 rounded-full animate-pulse bg-white dark:bg-blue-200"></div>

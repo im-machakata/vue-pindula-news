4af02785b0e9a433ff3fb7507279e51081442db2
Isaac Machakata <isaac@caasi.co.zw>
2023-04-04 04:47:42 +0200

    feature: add post info & icons

---
+++ src/components/News/Items.vue
@@ -0,0 +1,71 @@
+<template>
+  <section class="news-list">
+    <article class="news-item" v-for="( item, index) in news" :class="{ closed: !item.is_open }" :key="item.id"
+      @click="this.$emit('read-article', index)">
+      <img class="news-image" v-bind:src="item.image_src" v-bind:alt="item.image_caption" v-bind:title="item.slug">
+      <div class="details">
+        <a class="news-title" v-bind:title="item.title">{{ item.title }}</a>
+        <p class="text-sm font-sans">
+        <div class="flex flex-row flex-wrap my-1">
+          <div class="post-meta-info">
+            <i class="fa fa-user mr-1" aria-hidden="true"></i>
+            {{ item.author }}
+          </div>
+          <div class="post-meta-info">
+            <i class="fa fa-clock" aria-hidden="true"></i>
+            {{ item.published }}
+          </div>
+          <div class="post-meta-info">
+            <i class="fa fa-comments mr-1" aria-hidden="true"></i>
+            {{ item.comments }}
+          </div>
+          <div class="post-meta-info">
+            <i class="fa fa-tags mr-1" aria-hidden="true"></i>
+            <span v-html="getCategory(item.category.name)"></span>
+          </div>
+        </div>
+        </p>
+        <p class="text-sm font-sans excerpt flex flex-row flex-wrap">
+          <span v-html="item.excerpt"></span>
+        </p>
+      </div>
+    </article>
+  </section>
+</template>
+<script>
+export default {
+  name: 'NewsItem',
+  props: {
+    news: Array
+  },
+  methods: {
+    getCategory(value) {
+      if (value.includes("/")) {
+        value = value.split('/');
+        return value[0];
+      }
+      return value;
+    }
+  }
+}
+</script>
+<style scoped>
+img {
+  height: 180px;
+}
+
+.news-item.closed .news-title {
+  display: -webkit-box;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+}
+
+.news-item.closed .excerpt {
+  display: -webkit-box;
+  -webkit-line-clamp: 4;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+}
+</style>
+  

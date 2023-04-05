3cf500a5490115b029e1823688e2fc61862df43c
Isaac Machakata <isaac@caasi.co.zw>
2023-04-05 04:43:31 +0200

    backup

--- src/components/News/List.vue
+++ src/components/News/List.vue
@@ -3,19 +3,22 @@
   <section v-show="connection_error && !is_loading" class="text-white my-5 px-3 py-3 lg:text-center">
     An error occured connecting to the server. <a @click="load_news()" class="hover:underline">Retry</a>
   </section>
-  <NewsItem :news="latest_news" v-show="has_news" @read-article="toggle_expand_news"></NewsItem>
-  <NewsArticle v-show="read_article"></NewsArticle>
+  <NewsItems :news="latest_news" v-show="has_news" @read-article="read_article" @close-article="view_article = null"></NewsItems>
+  <NewsArticle v-show="view_article" :news="view_article"></NewsArticle>
+  <Pagination></Pagination>
 </template>
 <script>
 import NewsArticle from "./Article.vue"
-import NewsItem from "./Items.vue"
+import NewsItems from "./Items.vue"
 import Loader from "../Loader.vue"
+import Pagination from "../Pagination.vue"
 export default {
   name: 'NewsList',
   components: {
     Loader,
-    NewsItem,
+    NewsItems,
     NewsArticle,
+    Pagination,
   },
   computed: {
     has_news() {
@@ -23,20 +26,19 @@ export default {
     },
   },
   methods: {
-    toggle_expand_news(index, item) {
-      alert(`Working on displaying article`);
-      this.read_article = this.latest_news[index];
+    read_article(item) {
+      this.view_article = item;
     },
     load_news() {
       this.is_loading = true;
-      fetch(atob(this.latest_news_url))
+      fetch(atob(this.urls.news))
         .then(response => response.json())
         .then(response => {
           this.is_loading = false;
           this.latest_news = response.results.map(v => ({ ...v, is_open: false }));
           this.connection_error = false;
         })
-        .catch(err => {
+        .catch(() => {
           this.is_loading = false;
           this.connection_error = true;
         });
@@ -44,12 +46,14 @@ export default {
   },
   data() {
     return {
-      latest_news: Array(),
+      latest_news: [],
       is_loading: true,
+      view_article: false,
       connection_error: false,
-      read_article: false,
-      latest_news_url: 'aHR0cHM6Ly9hcGkuc2NyYXBpbmdhbnQuY29tL3YyL2dlbmVyYWw/dXJsPWh0dHBzJTNBJTJGJTJGemVyby5waW5kdWxhLmNvLnp3JTJGYXBpJTJGcG9zdHMmeC1hcGkta2V5PTc2MmIxMjcxMWM3MDRiMzZhZjRjZWZjMWU0OTM4MmExJmJyb3dzZXI9ZmFsc2U=',
-      ads_url: 'aHR0cHM6Ly9hcGkuc2NyYXBpbmdhbnQuY29tL3YyL2dlbmVyYWw/dXJsPWh0dHBzJTNBJTJGJTJGemVyby5waW5kdWxhLmNvLnp3JTJGYXBpJTJGcHJvZHVjdHMmeC1hcGkta2V5PTc2MmIxMjcxMWM3MDRiMzZhZjRjZWZjMWU0OTM4MmExJmJyb3dzZXI9ZmFsc2U='
+      urls: {
+        ads: 'aHR0cHM6Ly9hcGkuc2NyYXBpbmdhbnQuY29tL3YyL2dlbmVyYWw/dXJsPWh0dHBzJTNBJTJGJTJGemVyby5waW5kdWxhLmNvLnp3JTJGYXBpJTJGcHJvZHVjdHMmeC1hcGkta2V5PTc2MmIxMjcxMWM3MDRiMzZhZjRjZWZjMWU0OTM4MmExJmJyb3dzZXI9ZmFsc2U=',
+        news: 'aHR0cHM6Ly9hcGkuc2NyYXBpbmdhbnQuY29tL3YyL2dlbmVyYWw/dXJsPWh0dHBzJTNBJTJGJTJGemVyby5waW5kdWxhLmNvLnp3JTJGYXBpJTJGcG9zdHMmeC1hcGkta2V5PTc2MmIxMjcxMWM3MDRiMzZhZjRjZWZjMWU0OTM4MmExJmJyb3dzZXI9ZmFsc2U=',
+      }
     }
   },
   async created() {

885610720934cf43eeadf6735ff09055cf3d4209
Isaac Machakata <isaac@caasi.co.zw>
2023-04-05 14:59:53 +0200

    refactor: pagination

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -1,8 +1,8 @@
 <template>
     <section aria-label="Page navigation">
         <div class="flex justify-center mb-5">
-            <div class="pill" @click="this.$emit('previous-page')" v-show="previous">Previous Page</div>
-            <div class="pill" @click="this.$emit('next-page')" v-show="next">Next Page</div>
+            <div class="pill" @click="this.$emit('previous-page', previous)" v-show="previous">Previous Page</div>
+            <div class="pill" @click="this.$emit('next-page', next)" v-show="next">Next Page</div>
         </div>
     </section>
 </template>
@@ -10,8 +10,8 @@
 export default {
     name: 'Pagination',
     props: {
-        previous: Boolean,
-        next: Boolean,
+        previous: String,
+        next: String,
     },
     emits: ['previous-page', 'next-page']
 }

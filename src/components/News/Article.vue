3ac170a4b3c8f09865993eab659a7aede56ff9d1
Isaac Machakata <isaac@caasi.co.zw>
2023-04-06 01:28:29 +0200

    style: news blockquote & dialog

--- src/components/News/Article.vue
+++ src/components/News/Article.vue
@@ -2,13 +2,19 @@
   <aside class="read-article" @click="this.$emit('close-article')">
 
     <!-- Overlay -->
-    <div class="fixed z-40 w-screen h-screen inset-0 bg-gray-900 bg-opacity-60"></div>
+    <div class="fixed z-40 w-screen h-screen inset-0 bg-gray-900 bg-opacity-50"></div>
 
     <!-- The Dialog -->
-    <div class="dialog">
-      <h2 class="news-title text-blue-900 hover:text-blue-900" v-bind:title="news.title">{{ news.title }}</h2>
-      <a class="mb-2" :href="'//zero.pindula.co.zw/' + news.slug" target="_blank">zero.pindula.co.zw/{{ news.slug }}</a>
-      <p v-html="news.content"></p>
+    <div class="dialog mx-auto">
+      <div class="flex justify-center mb-2">
+        <div class="py-1 bg-blue-400 rounded-lg w-32"></div>
+      </div>
+      <h2 class="text-3xl" v-bind:title="news.title">{{ news.title }}</h2>
+      <a :href="'//zero.pindula.co.zw/' + news.slug" target="_blank">
+        https://zero.pindula.co.zw/{{ news.slug }}</a>
+      <div class="overflow-y-scroll max-w-full rounded-md pr-2 pb-8">
+        <p v-html="news.content" class="mt-4"></p>
+      </div>
     </div>
     <!-- <Comments></Comments> -->
   </aside>
@@ -26,7 +32,7 @@ export default {
 </script>
 <style scoped>
 .dialog {
-  height: 75vh;
-  max-height: 75vh;
+  height: 80vh;
+  max-height: 80vh;
 }
 </style>

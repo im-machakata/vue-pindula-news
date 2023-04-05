a3acaf2822c454414d045bc3f3adcf4ec005b90b
Isaac Machakata <isaac@caasi.co.zw>
2023-04-06 00:22:34 +0200

    refactor: menu

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,27 +1,21 @@
 <template>
   <nav class="mx-auto shadow-md border-y-2 border-blue-700 bg-blue-800 w-full py-4">
     <ul class="flex flex-row justify-around items-center lg:mr-10 font-semibold w-full font-sans cursor-pointer">
-      <li v-for="item in items" class="px-2 text-white"><a v-bind:href="item.href" target="_b"
-          @click="menuClicked($event, item.text)">{{ item.text }}</a></li>
+      <li v-for="item in items" class="px-2 text-white">
+        <a v-bind:href="item.href" target="_blank">{{ item.text }}</a>
+      </li>
     </ul>
   </nav>
 </template>
 <script>
 export default {
   name: 'Menu',
-  methods: {
-    menuClicked: (e, text) => {
-      if (text == "Home") {
-        e.preventDefault();
-      }
-    },
-  },
   data() {
     return {
       items: [
         {
           text: 'News',
-          href: '/',
+          href: '//zero.pindula.co.zw/',
         },
         {
           text: 'Jobs',
@@ -32,7 +26,6 @@ export default {
           href: '//zero.pindula.co.zw/market/',
         },
       ],
-      msg: null
     }
   },
 }

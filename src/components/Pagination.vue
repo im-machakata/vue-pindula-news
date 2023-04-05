decb194af60dae8682b87027a398a6fc6f2add2a
Isaac Machakata <isaac@caasi.co.zw>
2023-04-05 04:29:59 +0200

    refcator: paginations

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -1,34 +1,18 @@
 <template>
-    <section aria-label="Page navigation example">
-        <ul class="list-style-none flex">
-            <li>
-                <a
-                    class="pointer-events-none relative block rounded bg-transparent py-1.5 px-3 text-sm text-neutral-500 transition-all duration-300 dark:text-neutral-400">Previous</a>
-            </li>
-            <li>
-                <a class="relative block rounded bg-transparent py-1.5 px-3 text-sm text-neutral-600 transition-all duration-300 hover:bg-neutral-100  dark:text-white dark:hover:bg-neutral-700 dark:hover:text-white"
-                    href="#!">1</a>
-            </li>
-            <li aria-current="page">
-                <a class="relative block rounded bg-primary-100 py-1.5 px-3 text-sm font-medium text-primary-700 transition-all duration-300"
-                    href="#!">2
-                    <span
-                        class="absolute -m-px h-px w-px overflow-hidden whitespace-nowrap border-0 p-0 [clip:rect(0,0,0,0)]">(current)</span>
-                </a>
-            </li>
-            <li>
-                <a class="relative block rounded bg-transparent py-1.5 px-3 text-sm text-neutral-600 transition-all duration-300 hover:bg-neutral-100 dark:text-white dark:hover:bg-neutral-700 dark:hover:text-white"
-                    href="#!">3</a>
-            </li>
-            <li>
-                <a class="relative block rounded bg-transparent py-1.5 px-3 text-sm text-neutral-600 transition-all duration-300 hover:bg-neutral-100 dark:text-white dark:hover:bg-neutral-700 dark:hover:text-white"
-                    href="#!">Next</a>
-            </li>
-        </ul>
+    <section aria-label="Page navigation">
+        <div class="flex justify-center mb-5">
+            <div class="pill" @click="this.$emit('previous-page')" v-show="previous">Previous Page</div>
+            <div class="pill" @click="this.$emit('next-page')" v-show="next">Next Page</div>
+        </div>
     </section>
 </template>
 <script>
 export default {
-    name: 'Pagination'
+    name: 'Pagination',
+    props: {
+        previous: Boolean,
+        next: Boolean,
+    },
+    emits: ['previous-page', 'next-page']
 }
 </script>

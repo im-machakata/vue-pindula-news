7fb460b88ba2e6177ee13358970aaf9b2a41c93d
Isaac Machakata <isaac@caasi.co.zw>
2023-04-06 00:48:59 +0200

    style: pagination

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -1,6 +1,6 @@
 <template>
     <section aria-label="Page navigation">
-        <div class="flex justify-center mb-5">
+        <div class="flex justify-center mb-5 gap-8">
             <div class="pill" @click="this.$emit('previous-page', previous)" v-show="previous">Previous Page</div>
             <div class="pill" @click="this.$emit('next-page', next)" v-show="next">Next Page</div>
         </div>

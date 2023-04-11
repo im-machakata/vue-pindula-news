73bc4c582b689a07bd672942d5920cf0e4c5f920
Isaac Machakata <isaac@caasi.co.zw>
2023-04-11 07:30:46 +0200

    backup

--- src/components/News/Comments.vue
+++ src/components/News/Comments.vue
@@ -1,7 +1,6 @@
 <template>
     <aside class="comments text-white">
-        <a class="news-title"
-            v-bind:title="news.title">{{ news.title }}</a>
+        <a class="news-title" v-bind:title="news.title">{{ news.title }}</a>
         <p class="text-sm font-sans">
         <div class="flex flex-row flex-wrap my-1">
             <div class="post-meta-info">
@@ -30,9 +29,58 @@
 <script>
 export default {
     name: 'Comments',
-    props: ['list'],
+    props: ['slug'],
+    data() {
+        return {
+            urls: {
+                comments: 'aHR0cHM6Ly96ZXJvLnBpbmR1bGEuY28uencvYXBpL2Rpc2N1c3Npb25zL2NvbW1lbnRzP3ZlcnNpb249MyZzbHVnPQ==',
+                proxy: 'aHR0cHM6Ly9hcGkuc2NyYXBpbmdhbnQuY29tL3YyL2dlbmVyYWw/eC1hcGkta2V5PTc2MmIxMjcxMWM3MDRiMzZhZjRjZWZjMWU0OTM4MmExJmJyb3dzZXI9ZmFsc2UmdXJsPQ=='
+            }
+        }
+    },
+    methods: {
+        load_comments() {
+
+            // set url
+            url = atob(this.urls.proxy) + atob(this.urls.comments) + encodeURI(this.slug);
+
+            // fetch news items
+            fetch(url)
+
+                // convert result to json if successful
+                .then(response => response.json())
+
+                // process the results
+                .then(response => {
+
+                    // store response in the local variable
+                    this.latest_news = response;
+
+                    // add an is_open key 
+                    this.latest_news.results = response.results.map(v => ({ ...v, is_open: false }));
+
+                    // hide error message & loader
+                    this.connection_error = false;
+                    this.is_loading = false;
+
+                    // scroll to top
+                    window.scrollTo(0, 0);
+                })
+
+                // show error message
+                .catch(() => {
+
+                    // show error message & hide loader
+                    this.connection_error = true;
+                    this.is_loading = false;
+                });
+        }
+    },
     created: {
-        //https://zero.pindula.co.zw/api/discussions/comments?slug=article-slug-url&version=3
+        // fetch();
+        // aHR0cHM6Ly96ZXJvLnBpbmR1bGEuY28uencvYXBpL2Rpc2N1c3Npb25zL2NvbW1lbnRzP3NsdWc9
+        // https://zero.pindula.co.zw/api/discussions/comments?slug=article-slug-url&version=3
+        //this.load_comments();
     }
 }
 </script>

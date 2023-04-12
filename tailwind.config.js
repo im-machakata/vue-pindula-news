a9ace8482b08026fb8eda1ce32bf708009048320
Isaac Machakata <isaac@caasi.co.zw>
2023-04-12 04:12:47 +0200

    style: poppins font

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
   theme: {
     extend: {},
     fontFamily:{
-      'serif': ['"Newsreader"', ...defaultTheme.fontFamily.serif]
+      poppins: ['"Poppins"'],
     }
   },
   variants: {

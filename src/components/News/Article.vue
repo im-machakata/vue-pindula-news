c98addd239686a726b170f83f7b739bd91d50cdb
Isaac Machakata <isaac@caasi.co.zw>
2023-04-12 06:01:06 +0200

    style: scrollbars on chrome

--- src/components/News/Article.vue
+++ src/components/News/Article.vue
@@ -8,15 +8,15 @@
     <div class="dialog mx-auto lg:mx-2 pb-4">
       <div class="rounded-md overflow-y-scroll max-w-full">
         <div class="flex justify-center mb-2">
-        <div class="py-1 bg-blue-400 rounded-lg w-32"></div>
-      </div>
-      <h2 class="text-3xl" v-bind:title="news.title">{{ news.title }}</h2>
-      <a :href="'//zero.pindula.co.zw/' + news.slug" class="pb-2" target="_blank">
-        https://zero.pindula.co.zw/{{ news.slug }}</a>
-      <div class="pr-2">
-        <p v-html="news.content" class="the-news"></p>
-        <Comments :slug="news.slug"></Comments>
-      </div>
+          <div class="py-1 bg-blue-400 rounded-lg w-32"></div>
+        </div>
+        <h2 class="text-3xl" v-bind:title="news.title">{{ news.title }}</h2>
+        <a :href="'//zero.pindula.co.zw/' + news.slug" class="pb-2" target="_blank">
+          https://zero.pindula.co.zw/{{ news.slug }}</a>
+        <div class="pr-2">
+          <p v-html="news.content" class="the-news"></p>
+          <Comments :slug="news.slug"></Comments>
+        </div>
       </div>
     </div>
   </aside>
@@ -36,10 +36,5 @@ export default {
 .dialog {
   height: 80vh;
   max-height: 80vh;
-}
-.dialog .overflow-y-scroll::-webkit-scrollbar-track {
-  width: 6px;
-  border-radius: 8px;
-  background: #444;
 }
 </style>

0c94bb10bb482581e749c13c2a5f01fa162a6d8e
Isaac Machakata <isaac@caasi.co.zw>
2023-04-12 06:41:12 +0200

    style: comments threads

--- src/components/Comments/Item.vue
+++ src/components/Comments/Item.vue
@@ -6,14 +6,14 @@
         <div class="comment mb-4 border-b pb-2">{{ comment.text }}</div>
         <div class="replies" v-if="comment.children.length > 0">
             <div class="reply-one" v-for="child in comment.children">
-                <div class="comment ml-4">
+                <div class="comment ml-6">
                     <div class="name flex">
                         <h3 class="font-bold mr-1">{{ child.name }}</h3> &ndash; {{ child.created_on }}
                     </div>
                     <div class="comment mb-4 border-b pb-2">{{ child.text }}</div>
                     <div class="replies" v-if="child.children.length > 0">
-                        <div class="reply-one" v-for="muzukuru in comment.children">
-                            <div class="comment ml-4">
+                        <div class="reply-one" v-for="muzukuru in child.children">
+                            <div class="comment ml-6">
                                 <div class="name flex">
                                     <h3 class="font-bold mr-1">{{ muzukuru.name }}</h3> &ndash; {{ muzukuru.created_on }}
                                 </div>

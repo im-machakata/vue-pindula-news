22fdbb71091a293ca044ab4e02d3ccab974db943
Isaac Machakata <isaac@caasi.co.zw>
2023-04-12 08:51:10 +0200

    style: muzukur-comment :)

--- src/components/Comments/Item.vue
+++ src/components/Comments/Item.vue
@@ -1,11 +1,12 @@
 <template>
     <div class="comment-container">
         <div class="comment-parent" :class="{ 'rounded-br-none': comment.children.length > 0 }">
-            <div class="name">
+            <div class="name pl-1">
                 <span class="font-bold mr-1">{{ comment.name }}</span>
+                &ndash;
                 <span class="text-neutral-700 ml-1">{{ comment.created_on }}</span>
             </div>
-            <div class="comment pb-2">{{ comment.text }}</div>
+            <div class="comment pb-3 pl-1">{{ comment.text }}</div>
         </div>
         <div class="comment-replies" v-if="comment.children.length > 0">
             <div class="reply-one" v-for="child in comment.children">
@@ -16,9 +17,9 @@
                     </div>
                     <div class="pt-1 comment mb-4">{{ child.text }}</div>
                     <div class="replies mb-3 ml-6" v-if="child.children.length > 0">
-                        <div class="reply rounded border" v-for="muzukuru in child.children">
+                        <div class="reply mr-0 border-l" v-for="muzukuru in child.children">
                             <div class="comment ml-2">
-                                <div class="name pt-2">
+                                <div class="name">
                                     <span class="font-bold mr-1">{{ muzukuru.name }}</span> &ndash; <span
                                         class="text-neutral-700">{{ muzukuru.created_on }}</span>
                                 </div>

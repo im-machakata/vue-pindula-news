6ae3a4b6408b20d746718483f3496c39be2ca1bc
Isaac Machakata <isaac@caasi.co.zw>
2023-04-12 06:05:24 +0200

    style: extra space

--- src/components/News/Article.vue
+++ src/components/News/Article.vue
@@ -5,13 +5,13 @@
     <div class="fixed z-40 w-screen h-screen inset-0 bg-gray-900 bg-opacity-50"></div>
 
     <!-- The Dialog -->
-    <div class="dialog mx-auto lg:mx-2 pb-4">
+    <div class="dialog mx-auto lg:mx-4 pb-4">
       <div class="rounded-md overflow-y-scroll max-w-full">
         <div class="flex justify-center mb-2">
           <div class="py-1 bg-blue-400 rounded-lg w-32"></div>
         </div>
         <h2 class="text-3xl" v-bind:title="news.title">{{ news.title }}</h2>
-        <a :href="'//zero.pindula.co.zw/' + news.slug" class="pb-2" target="_blank">
+        <a :href="'//zero.pindula.co.zw/' + news.slug" class="pb-4" target="_blank">
           https://zero.pindula.co.zw/{{ news.slug }}</a>
         <div class="pr-2">
           <p v-html="news.content" class="the-news"></p>

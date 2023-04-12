2cbadfb9cb66642ed697e13523f46539baa8065c
Isaac Machakata <isaac@caasi.co.zw>
2023-04-12 05:58:34 +0200

    style: comments

--- src/components/Comments/Item.vue
+++ src/components/Comments/Item.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="comment">
         <h3 class="name font-bold">{{ comment.name }}</h3>
-        <div class="comment mb-4">{{ comment.text }}</div>
+        <div class="comment mb-4 border-b pb-2">{{ comment.text }}</div>
     </div>
 </template>
 <script>
--- src/components/News/Article.vue
+++ src/components/News/Article.vue
@@ -5,7 +5,7 @@
     <div class="fixed z-40 w-screen h-screen inset-0 bg-gray-900 bg-opacity-50"></div>
 
     <!-- The Dialog -->
-    <div class="dialog mx-auto lg:mx-2">
+    <div class="dialog mx-auto lg:mx-2 pb-4">
       <div class="rounded-md overflow-y-scroll max-w-full">
         <div class="flex justify-center mb-2">
         <div class="py-1 bg-blue-400 rounded-lg w-32"></div>
@@ -13,8 +13,8 @@
       <h2 class="text-3xl" v-bind:title="news.title">{{ news.title }}</h2>
       <a :href="'//zero.pindula.co.zw/' + news.slug" class="pb-2" target="_blank">
         https://zero.pindula.co.zw/{{ news.slug }}</a>
-      <div class="pr-2 pb-8">
-        <p v-html="news.content"></p>
+      <div class="pr-2">
+        <p v-html="news.content" class="the-news"></p>
         <Comments :slug="news.slug"></Comments>
       </div>
       </div>
@@ -36,5 +36,10 @@ export default {
 .dialog {
   height: 80vh;
   max-height: 80vh;
+}
+.dialog .overflow-y-scroll::-webkit-scrollbar-track {
+  width: 6px;
+  border-radius: 8px;
+  background: #444;
 }
 </style>

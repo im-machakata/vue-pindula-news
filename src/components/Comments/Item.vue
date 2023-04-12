9bf7be051b88829221d438d61d600ae5280fc14e
Isaac Machakata <isaac@caasi.co.zw>
2023-04-12 07:24:28 +0200

    style: comments

--- src/components/Comments/Item.vue
+++ src/components/Comments/Item.vue
@@ -1,23 +1,25 @@
 <template>
-    <div class="comment">
-        <div class="name flex">
-            <h3 class="font-bold mr-1">{{ comment.name }}</h3> &ndash; {{ comment.created_on }}
+    <div class="comment-list">
+        <div class="comment border px-2 pt-3 rounded mb-4">
+            <div class="name">
+                <span class="font-bold mr-1">{{ comment.name }}</span> &ndash; <span class="text-neutral-700">{{ comment.created_on }}</span>
+            </div>
+            <div class="comment pb-2">{{ comment.text }}</div>
         </div>
-        <div class="comment mb-4 border-b pb-2">{{ comment.text }}</div>
-        <div class="replies" v-if="comment.children.length > 0">
+        <div class="replies border-l border-b border-r px-2 pt-3 -mt-5 ml-9 mb-4 rounded" v-if="comment.children.length > 0">
             <div class="reply-one" v-for="child in comment.children">
-                <div class="comment ml-6">
-                    <div class="name flex">
-                        <h3 class="font-bold mr-1">{{ child.name }}</h3> &ndash; {{ child.created_on }}
+                <div class="comment ml-2">
+                    <div class="name">
+                        <span class="font-bold mr-1">{{ child.name }}</span> &ndash; <span class="text-neutral-700">{{ child.created_on }}</span>
                     </div>
-                    <div class="comment mb-4 border-b pb-2">{{ child.text }}</div>
-                    <div class="replies" v-if="child.children.length > 0">
-                        <div class="reply-one" v-for="muzukuru in child.children">
-                            <div class="comment ml-6">
-                                <div class="name flex">
-                                    <h3 class="font-bold mr-1">{{ muzukuru.name }}</h3> &ndash; {{ muzukuru.created_on }}
+                    <div class="pt-1 comment mb-4">{{ child.text }}</div>
+                    <div class="replies mb-3 ml-6" v-if="child.children.length > 0">
+                        <div class="reply rounded border" v-for="muzukuru in child.children">
+                            <div class="comment ml-2">
+                                <div class="name pt-2">
+                                    <span class="font-bold mr-1">{{ muzukuru.name }}</span> &ndash; <span class="text-neutral-700">{{ muzukuru.created_on }}</span>
                                 </div>
-                                <div class="comment mb-4 border-b pb-2">{{ muzukuru.text }}</div>
+                                <div class="comment mb-2">{{ muzukuru.text }}</div>
                             </div>
                         </div>
                     </div>
